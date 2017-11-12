4ddbef8ed6428f286db54575d024f37973f85a1d
Marc Bojakowski <29601710+mbojakowski@users.noreply.github.com>
2017-11-12 13:21:22 +0100

    feat: enable generating basic static websites

---
+++ README.md
@@ -0,0 +1,16 @@
+# swgen
+A powerful static website generator for Linux
+
+## Quick start
+
+    build.sh
+    build.sh --watch
+
+The `--watch` option requires `inotifywait` to be installed:
+
+    sudo apt-get install inotify-tools
+
+## Disclaimer
+
+I like using a small knife to cut apples, a saw to cut wood, and a nail clipper to cut nails.
+This tool is a very basic generator that fulfills my current needs, no more no less. It is however built upon proven tools computers have been running on for decades. If it can be useful to anybody, please help yourself.
---
+++ build.sh
@@ -0,0 +1,71 @@
+BASE_PATH=`dirname $0`
+
+DEFAULT_TEMPLATE=posts.html
+TARGET_DIR=$BASE_PATH/target
+BASE_FILE=index.html
+
+create_target_dir_if_necessary() {
+    if [ ! -d "$BASE_PATH/target" ]; then
+        echo "Creating target directory"
+        mkdir "$TARGET_DIR"
+    fi
+}
+
+clean() {
+    if [ -d "$TARGET_DIR" ]; then
+        echo "Removing target directory"
+        rm -r "$TARGET_DIR"
+    fi
+}
+
+create_target_files() {
+    for file in $BASE_PATH/*.html; do
+        create_target_file $file
+    done
+}
+
+create_target_file() {
+    TEMPLATE_FILE=$1
+    if [[ $1 == *$BASE_FILE ]]; then
+        TEMPLATE_FILE=$DEFAULT_TEMPLATE
+    fi
+    TITLE=`sed -rn '1,1s/.*title=(.+)-->/\1/p' $BASE_PATH/$TEMPLATE_FILE`
+    TEMPLATE_CONTENT=`sed -n '1,1!p' $BASE_PATH/$TEMPLATE_FILE | tr -d '\n'`
+    sed -r "s/\{\{title\}\}/$TITLE/ ; s#\{\{content\}\}#$TEMPLATE_CONTENT#" $BASE_PATH/index.html > $TARGET_DIR/$1
+}
+
+copy_resources() {
+    for style in $BASE_PATH/*.css; do
+        cp $style $TARGET_DIR
+    done
+    if [ -d "$BASE_PATH/fonts" ]; then
+        cp -r $BASE_PATH/fonts $TARGET_DIR/fonts
+    fi
+}
+
+all() {
+    clean
+    create_target_dir_if_necessary
+    create_target_files
+    copy_resources
+}
+
+on_change() {
+    if [[ $1 =~ .*\.(html|css)$ ]]; then
+        echo "File $1 changed; rebuilding..."
+        all
+    else
+        echo "Ignoring modified file $1"
+    fi
+}
+
+if [[ $1 == "--watch" ]]; then
+    all
+    echo "Waiting for changes..."
+    while RES=`inotifywait --quiet --event modify --format %f $BASE_PATH`; do
+        on_change $RES
+        echo "Waiting for changes..."
+    done
+else
+    all
+fi
